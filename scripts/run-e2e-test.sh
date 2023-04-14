dcd0d72c61b08c51c6c2ed96a9314b2949cdfe97
Zeyu Li <lizeyu@amazon.com>
2023-04-14 12:37:14 -0700

    add test suite

--- scripts/run-e2e-test.sh
+++ scripts/run-e2e-test.sh
@@ -4,3 +4,6 @@ source .circleci/local_publish_helpers.sh
 export PATH=$AMPLIFY_DIR:$PATH
 echo "using Amplify CLI version: "$(amplify --version)
 amplify --version
+echo "Run Amplify E2E tests"
+echo $TEST_SUITE
+retry runE2eTest
