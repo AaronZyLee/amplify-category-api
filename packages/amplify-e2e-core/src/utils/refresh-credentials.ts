282aefe2f64de9997721dc6f52829f517c76ae62
phani-srikar <phanisrikar93ume@gmail.com>
2023-06-01 18:55:31 -0700

    test: remove function in script

--- packages/amplify-e2e-core/src/utils/refresh-credentials.ts
+++ packages/amplify-e2e-core/src/utils/refresh-credentials.ts
@@ -2,18 +2,14 @@ const { execSync } = require("child_process");
 import { resolve } from 'path';
 import { existsSync } from 'fs-extra';
 
-let commandOne = "";
-let commandTwo = "_refreshCredentials"; // print the current user
-let commandThree = "pwd"; //print the name of current directory
-
 export const refreshCredentials = () => {
-  const pathToScript = resolve(`${__dirname}/../../../..`);
-  console.log(pathToScript);
-  const pathToSourceDir = process.env.CODEBUILD_SRC_DIR;
-  console.log(`script path exists: ${existsSync(pathToScript)}`);
-  console.log(`src path exists: ${existsSync(pathToSourceDir)}`);
-  const pathToBash = execSync('which bash').toString();
-  console.log(`Found bash: ${pathToBash}`);
+  // const pathToScript = resolve(`${__dirname}/../../../..`);
+  // console.log(pathToScript);
+  // const pathToSourceDir = process.env.CODEBUILD_SRC_DIR;
+  // console.log(`script path exists: ${existsSync(pathToScript)}`);
+  // console.log(`src path exists: ${existsSync(pathToSourceDir)}`);
+  // const pathToBash = execSync('which bash').toString();
+  // console.log(`Found bash: ${pathToBash}`);
   const listCurrFiles = execSync('pwd');
   console.log(listCurrFiles?.toString());
   console.log(execSync('ls').toString());
@@ -23,19 +19,3 @@ export const refreshCredentials = () => {
   console.log(JSON.stringify(result));
   return;
 };
-
-/*
-export const refreshCredentials = (cwd: string)
-: Promise<any> => new Promise((resolve, reject) => {
-  console.log(`running from ${cwd}`);
-
-  spawn('./shared-scripts.sh', ['_refreshCredentials'], { cwd: '/Users/edupp/Documents/git_repos/git_data/cb-git/amplify-category-api', stripColors: true })
-    .run((err: Error) => {
-      if (!err) {
-        resolve({});
-      } else {
-        reject(err);
-      }
-    });
-});
-*/
--- packages/amplify-e2e-tests/refresh-credentials.sh
+++ packages/amplify-e2e-tests/refresh-credentials.sh
@@ -3,54 +3,45 @@
 # set exit on error to true
 set -e
 
-function _unassumeTestAccountCredentials {
-    echo "Unassume Role"
-    unset AWS_ACCESS_KEY_ID
-    unset AWS_SECRET_ACCESS_KEY
-    unset AWS_SESSION_TOKEN
-}
+echo "Environment credentials"
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
+echo $AWS_SESSION_TOKEN
 
-function _checkEnvCreds {
-    echo "Environment credentials"
-    echo $AWS_ACCESS_KEY_ID
-    echo $AWS_SECRET_ACCESS_KEY
-    echo $AWS_SESSION_TOKEN
-}
+echo "User Identity Account"
+echo $(aws sts get-caller-identity | jq -cr '.Account')
 
-function _logUserIdentity {
-    echo "User Identity Account"
-    echo $(aws sts get-caller-identity | jq -cr '.Account')
-}
+echo "Refreshing temporary credentials"
+echo "Unassume Role"
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
 
-function _refreshCredentials {
-    echo "Refreshing temporary credentials"
-    _checkEnvCreds
-    _logUserIdentity
-    _unassumeTestAccountCredentials
+echo "Environment credentials"
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
+echo $AWS_SESSION_TOKEN
 
-    echo "After clearing current credentials"
-    _checkEnvCreds
-    _logUserIdentity
-    _loadTestAccountCredentials
+echo "User Identity Account"
+echo $(aws sts get-caller-identity | jq -cr '.Account')
 
-    echo "After resetting temp credentials"
-    _checkEnvCreds
-    _logUserIdentity
-}
+echo "ASSUMING PARENT TEST ACCOUNT credentials"
+session_id=1248634
+# Use longer time for parent account role
+creds=$(aws sts assume-role --role-arn $TEST_ACCOUNT_ROLE --role-session-name testSession${session_id} --duration-seconds 3600)
+if [ -z $(echo $creds | jq -c -r '.AssumedRoleUser.Arn') ]; then
+    echo "Unable to assume parent e2e account role."
+    return
+fi
+echo "Using account credentials for $(echo $creds | jq -c -r '.AssumedRoleUser.Arn')"
+export AWS_ACCESS_KEY_ID=$(echo $creds | jq -c -r ".Credentials.AccessKeyId")
+export AWS_SECRET_ACCESS_KEY=$(echo $creds | jq -c -r ".Credentials.SecretAccessKey")
+export AWS_SESSION_TOKEN=$(echo $creds | jq -c -r ".Credentials.SessionToken")
 
-function _loadTestAccountCredentials {
-    echo ASSUMING PARENT TEST ACCOUNT credentials
-    session_id=$((1 + $RANDOM % 10000))
-    # Use longer time for parent account role
-    creds=$(aws sts assume-role --role-arn $TEST_ACCOUNT_ROLE --role-session-name testSession${session_id} --duration-seconds 3600)
-    if [ -z $(echo $creds | jq -c -r '.AssumedRoleUser.Arn') ]; then
-        echo "Unable to assume parent e2e account role."
-        return
-    fi
-    echo "Using account credentials for $(echo $creds | jq -c -r '.AssumedRoleUser.Arn')"
-    export AWS_ACCESS_KEY_ID=$(echo $creds | jq -c -r ".Credentials.AccessKeyId")
-    export AWS_SECRET_ACCESS_KEY=$(echo $creds | jq -c -r ".Credentials.SecretAccessKey")
-    export AWS_SESSION_TOKEN=$(echo $creds | jq -c -r ".Credentials.SessionToken")
-}
+echo "Environment credentials"
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
+echo $AWS_SESSION_TOKEN
 
-_refreshCredentials
+echo "User Identity Account"
+echo $(aws sts get-caller-identity | jq -cr '.Account')
