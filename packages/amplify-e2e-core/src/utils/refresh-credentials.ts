da988eeb9f2d09bf28afa1ddb3eebd1829fef020
phani-srikar <phanisrikar93ume@gmail.com>
2023-06-02 13:44:48 -0700

    test: update shared e2e profile

--- packages/amplify-e2e-core/src/utils/refresh-credentials.ts
+++ packages/amplify-e2e-core/src/utils/refresh-credentials.ts
@@ -1,5 +1,8 @@
 const { execSync } = require("child_process");
 import _ from "lodash";
+import { writeFileSync, readFileSync } from "fs-extra";
+import * as ini from 'ini';
+import { pathManager } from "@aws-amplify/amplify-cli-core";
 
 export const refreshCredentials = () => {
   const stringifedCreds = execSync(`./refresh-credentials.sh`, { shell: true })?.toString();
@@ -7,9 +10,32 @@ export const refreshCredentials = () => {
   if (!_.isEmpty(stringifedCreds)) {
     const creds = JSON.parse(stringifedCreds);
     if (creds?.AWS_ACCESS_KEY_ID && creds?.AWS_SECRET_ACCESS_KEY && creds?.AWS_SESSION_TOKEN) {
+      refreshTestProfileCredentials(creds);
       return creds;
     }
   }
 
   return;
+};
+
+export const refreshTestProfileCredentials = (creds: AWSTempCredentials) => {
+  const profileName = 'amplify-integ-test-user';
+  const credentialsContents = ini.parse(readFileSync(pathManager.getAWSCredentialsFilePath()).toString());
+  credentialsContents[profileName] = credentialsContents[profileName] || {};
+  credentialsContents[profileName].aws_access_key_id = creds.AWS_ACCESS_KEY_ID;
+  credentialsContents[profileName].aws_secret_access_key = creds.AWS_SECRET_ACCESS_KEY;
+  credentialsContents[profileName].aws_session_token = creds.AWS_SESSION_TOKEN;
+  writeFileSync(pathManager.getAWSCredentialsFilePath(), ini.stringify(credentialsContents));
+};
+
+export type AWSTempCredentials = {
+  AWS_ACCESS_KEY_ID: string,
+  AWS_SECRET_ACCESS_KEY: string,
+  AWS_SESSION_TOKEN: string
+};
+
+export const displaySharedCreds = () => {
+  const profileName = 'amplify-integ-test-user';
+  const credentialsContents = ini.parse(readFileSync(pathManager.getAWSCredentialsFilePath()).toString());
+  console.log(`shared creds: ${ini.stringify(credentialsContents[profileName])}`)
 };
--- packages/amplify-e2e-tests/src/__tests__/transformer-migrations/searchable-migration.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/transformer-migrations/searchable-migration.test.ts
@@ -8,7 +8,7 @@ import {
   addAuthWithDefault,
 } from 'amplify-category-api-e2e-core';
 import { addApiWithoutSchema, updateApiSchema, getProjectMeta } from 'amplify-category-api-e2e-core';
-import { createNewProjectDir, deleteProjectDir, refreshCredentials } from 'amplify-category-api-e2e-core';
+import { createNewProjectDir, deleteProjectDir, refreshCredentials, displaySharedCreds } from 'amplify-category-api-e2e-core';
 import gql from 'graphql-tag';
 import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
 (global as any).fetch = require('node-fetch');
@@ -149,6 +149,7 @@ const displaySessionInfo = () => {
     acskey: process.env.AWS_ACCESS_KEY_ID
   };
   console.log(JSON.stringify(envVars));
+  displaySharedCreds();
 };
 
 const setCredsInEnv = (creds) => {
