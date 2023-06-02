a49b65dfed23f4e27ccae43fe44360780f15080a
phani-srikar <phanisrikar93ume@gmail.com>
2023-06-02 09:43:38 -0700

    chore: re-run test

--- packages/amplify-e2e-core/src/utils/refresh-credentials.ts
+++ packages/amplify-e2e-core/src/utils/refresh-credentials.ts
@@ -2,10 +2,7 @@ const { execSync } = require("child_process");
 import _ from "lodash";
 
 export const refreshCredentials = () => {
-  console.log(execSync('ls').toString());
-
   const stringifedCreds = execSync(`./refresh-credentials.sh`, { shell: true })?.toString();
-  console.log(`creds from js process: ${stringifedCreds}`);
 
   if (!_.isEmpty(stringifedCreds)) {
     const creds = JSON.parse(stringifedCreds);
@@ -13,13 +10,6 @@ export const refreshCredentials = () => {
       return creds;
     }
   }
-
-  const result1 = execSync(`
-  echo $AWS_ACCESS_KEY_ID
-  echo $AWS_SECRET_ACCESS_KEY
-  echo $AWS_SESSION_TOKEN
-  `, { shell: true });
-  console.log(result1?.toString());
 
   return;
 };
