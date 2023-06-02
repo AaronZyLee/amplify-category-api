2fb2ef90614be52023f87c04cb928e43d81c32f4
phani-srikar <phanisrikar93ume@gmail.com>
2023-06-01 17:26:42 -0700

    test: remove source cmd

--- packages/amplify-e2e-core/src/utils/refresh-credentials.ts
+++ packages/amplify-e2e-core/src/utils/refresh-credentials.ts
@@ -18,7 +18,7 @@ export const refreshCredentials = () => {
   console.log(listCurrFiles?.toString());
   console.log(execSync('ls').toString());
 
-  const result = execSync(`cd ${pathToSourceDir} && source ./shared-scripts.sh && _refreshCredentials`, { shell: true });
+  const result = execSync(`cd ${pathToSourceDir} && . shared-scripts.sh && _refreshCredentials`, { shell: true });
   console.log(result?.toString());
   console.log(JSON.stringify(result));
   return;
