d59597b84d458bf7fb7aa7ae0448eabaac1e9128
phani-srikar <phanisrikar93ume@gmail.com>
2023-06-02 21:31:19 -0700

    chore: fix random num

--- packages/amplify-e2e-tests/refresh-credentials.sh
+++ packages/amplify-e2e-tests/refresh-credentials.sh
@@ -9,7 +9,7 @@ unset AWS_SECRET_ACCESS_KEY
 unset AWS_SESSION_TOKEN
 
 # Re-assume the e2e test account role
-session_id=$((1 + $RANDOM % 10000))
+session_id=$(od -An -N2 -i /dev/urandom | awk '{print $1}' | sed 's/^0*//' | head -n 1)
 creds=$(aws sts assume-role --role-arn $TEST_ACCOUNT_ROLE --role-session-name testSession${session_id} --duration-seconds 3600)
 if [ -z $(echo $creds | jq -c -r '.AssumedRoleUser.Arn') ]; then
     return
